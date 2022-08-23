360141a888d7e7df1bf8bf08ac65bbf001e2ed6d
Sumingcheng <fanxiaohaohaha@qq..com>
2022-08-23 20:37:49 +0800

    INIT

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    {{ reactiveData.data }}
+    <button @click="changeData">+5</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+// reactive 响应式数据
+import {reactive} from "vue";
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    let reactiveData = reactive({
+      data: 10
+    })
+    const changeData = () => {
+      reactiveData.data += 5
+    }
+    return {reactiveData, changeData}
+  }
+}
+</script>
